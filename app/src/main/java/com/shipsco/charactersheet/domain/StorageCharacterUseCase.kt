23caa8245b93f40d25d3061344f15b66d01c8990
CinnaKenToast <rockmankim@gmail.com>
2022-08-06 00:04:44 -0500

    create all functions with files

--- app/src/main/java/com/shipsco/charactersheet/domain/StorageCharacterUseCase.kt
+++ app/src/main/java/com/shipsco/charactersheet/domain/StorageCharacterUseCase.kt
@@ -1,25 +1,50 @@
 package com.shipsco.charactersheet.domain
 
+import android.content.Context
 import com.shipsco.charactersheet.data.character.Character
+import com.shipsco.charactersheet.utils.fromJsonString
+import com.shipsco.charactersheet.utils.toJsonString
+import java.io.File
 
-class StorageCharacterUseCase: CharacterInterface {
+class StorageCharacterUseCase(
+    private val context: Context
+): CharacterInterface {
     override suspend fun getCharacters(): List<Character> {
         TODO("Not yet implemented")
     }
 
     override suspend fun getCharacter(name: String): Result<Character> {
-        TODO("Not yet implemented")
+        val file = File(context.filesDir, "${name}.char")
+        val characterJson = file.readText()
+        return if ( characterJson.isBlank() ) {
+            Result.failure(Throwable("Could not find character: $name"))
+        } else {
+            Result.success(characterJson.fromJsonString())
+        }
     }
 
     override suspend fun addCharacter(character: Character) {
-        TODO("Not yet implemented")
+        val characterJson = character.toJsonString()
+        val file = File(context.filesDir, "${character.characterName}.char")
+        file.writeText(characterJson)
     }
 
     override suspend fun removeCharacter(character: Character) {
-        TODO("Not yet implemented")
+        val file = File(context.filesDir, "${character.characterName}.char")
+        file.delete()
     }
 
     override suspend fun updateCharacter(character: Character) {
-        TODO("Not yet implemented")
+        val characterJson = character.toJsonString()
+        val file = File(context.filesDir, "${character.characterName}.char")
+        file.writeText(characterJson)
+    }
+
+    companion object {
+        fun createInstance(context: Context): StorageCharacterUseCase {
+            return StorageCharacterUseCase(
+                context = context
+            )
+        }
     }
 }
